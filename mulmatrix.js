445a89faa988c58ba4a6a4fdac632e55c4ce6d6e
Mateus <mateus@MBP-de-Mateus.lan>
2019-03-12 08:26:16 +0000

    JS

---
+++ mulmatrix.js
@@ -0,0 +1,31 @@
+const matrixMult = (matrixA, matrixB) => {
+  let finalMatrix = [];
+
+  for (let i = 0; i < matrixA.length; i++) {
+    const lineA = matrixA[i];
+    let newLine = [];
+
+    for (let j = 0; j < matrixB.length; j++) {
+      const lineB = matrixB[j];
+
+      let newElement = 0;
+      for (let k = 0; k < lineB.length; k++) {
+        const elementA = lineA[j];
+        const elementB = lineB[k];
+        newElement += elementA * elementB;
+      }
+      newLine.push(newElement);
+    }
+
+    finalMatrix.push(newLine);
+  }
+
+  return finalMatrix;
+};
+
+const matrixA = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
+const matrixB = [[2, 2, 2], [2, 2, 2], [2, 2, 2]];
+
+const result = matrixMult(matrixA, matrixB);
+
+console.log(result);
