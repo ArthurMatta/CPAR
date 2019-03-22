ea5f8cdea95db225272d35194fa5d833ff2f671d
Mateus <mpcmarques@icloud.com>
2019-03-22 21:19:35 +0000

    variables name change :art:

--- js/blockLineMultMatrix.js
+++ js/blockLineMultMatrix.js
@@ -1,7 +1,7 @@
-const blockLineMultMatrix = (B, C, bsize) => {
-  const size = B.length;
+const blockLineMultMatrix = (m1, m2, bsize) => {
+  const size = m1.length;
 
-  let A = Array(size).fill(Array(size).fill(0));
+  let matrix = Array(size).fill(Array(size).fill(0));
 
   for (i1 = 0; i1 < size; i1 += bsize) {
     for (j1 = 0; j1 < size; j1 += bsize) {
@@ -9,7 +9,7 @@ const blockLineMultMatrix = (B, C, bsize) => {
         for (i = parseInt(i1); i < i1 + bsize && i < size; i++) {
           for (j = parseInt(j1); j < j1 + bsize && j < size; j++) {
             for (k = parseInt(k1); k < k1 + bsize && k < size; k++) {
-              A[i][k] += B[i][j] * C[j][k];
+              matrix[i][k] += m1[i][j] * m2[j][k];
             }
           }
         }
@@ -17,7 +17,7 @@ const blockLineMultMatrix = (B, C, bsize) => {
     }
   }
 
-  return A;
+  return matrix;
 };
 
 module.exports = blockLineMultMatrix;
